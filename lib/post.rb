5272fe440227e020dc5a9871b39f6a330cb3c458
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-27 16:43:14 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post 
+  attr_accessor :name, :author
+  @@all = []
+  
+  def initialize(name, author=nil)
+  @name = name 
+  @author = author  
+  @@all << self
+  end 
+  def author_name
+    if self.author
+      self.author.name
+    else nil
+    end 
+  end  
+end 
